f5225238f537862bf8165514ab35cf7110d213b8
rpa216 <raj55.patel@gmail.com>
2017-11-29 21:34:41 -0500

    Create Arrays - DS.py

---
+++ Arrays - DS.py
@@ -0,0 +1,46 @@
+
+An array is a type of data structure that stores elements of the same type in a contiguous block of memory. In an array, , of size , each memory location has some unique index,  (where ), that can be referenced as  (you may also see it written as ).
+
+Given an array, , of  integers, print each element in reverse order as a single line of space-separated integers.
+
+Note: If you've already solved our C++ domain's Arrays Introduction challenge, you may want to skip this.
+
+Input Format
+
+The first line contains an integer,  (the number of integers in ). 
+The second line contains  space-separated integers describing .
+
+Constraints
+
+Output Format
+
+Print all  integers in  in reverse order as a single line of space-separated integers.
+
+Sample Input
+
+4
+1 4 3 2
+Sample Output
+
+2 3 4 1
+
+
+
+
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+for i in range(0, n//2):
+    temp = arr[i]
+    arr[i] = arr[-1-i]
+    arr[-1-i] = temp
+    
+result = ""
+for i in arr:
+    result = result + str(i) + " "
+
+print(result)
